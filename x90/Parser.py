a1b6aba1c95601f7fbd3fbfbc0b0b67185a5665f
Nicolas Broeking <nbroeking@me.com>
2015-01-14 22:52:47 -0700

    First Skeleton

---
+++ x90/Parser.py
@@ -0,0 +1,8 @@
+#This class provides all functionality for parsing a python file
+
+class Parser:
+	def __init__(self):
+		print("Created Parser")
+
+	def parse(self, file ):
+		print("Parsing the file: " + file)
