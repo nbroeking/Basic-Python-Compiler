f77c87148b93a57b17901e625e83c9dea06f3a2e
Nicolas Broeking <nbroeking@me.com>
2015-01-15 10:31:49 -0700

    Added Git ignore

--- backslashx90/Parser.py
+++ backslashx90/Parser.py
@@ -1,8 +1,10 @@
 #This class provides all functionality for parsing a python file
 
+from compiler import *
+
 class Parser:
 	def __init__(self):
 		print("Created Parser")
 
 	def parse(self, file ):
-		print("Parsing the file: " + file)
+		return parse('3 + 4 + 5');
--- backslashx90/compile.py
+++ backslashx90/compile.py
@@ -1,12 +1,12 @@
 #Main Python Module
 #Written By Nicolas Broeking and Josh Rahm
-#Team: x90
+#Team: backslashx90
 
 from Parser import Parser
 
 def main():
 	p = Parser();
-	p.parse("test1.py");
+	print p.parse("test1.py");
 
 if __name__ == "__main__":
     main()
