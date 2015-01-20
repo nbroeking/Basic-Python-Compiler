e0cd616b6e195cdee74e1ca11649c7dbe30c3908
nbroeking <nbroeking@me.com>
2015-01-20 11:48:50 -0700

    Running Test Suit

--- backslashx90/tests/generateTests.sh
+++ backslashx90/tests/generateTests.sh
@@ -17,23 +17,34 @@ os=`uname`
 
 # some commonly used files to generate
 echo '#!/bin/bash'>&3
-echo 'echo '\''Running the test suit how did you do?'\'''>&3
+echo "red=\$(echo -ne '\\e[01;31m')">&3
+echo "green=\$(echo -ne '\\e[01;32m')">&3
+echo "cyan=\$(echo -ne '\\e[01;36m')">&3
+echo "nc=\$(echo -ne ' \\e[00;0m')">&3
+echo 'echo -e '\"'\n${cyan}Running the test suit how did you do? ${nc}'\"''>&3
 echo -e "\n">&3
 # iterate through all of the tests and 
 # run them
 for ((i=0;i<${#src[@]};i++)) ; do
-    echo "echo \"Running Test: ${src[$i]}\"">&3
+    echo "echo \" \${cyan}Running Test: $i \${nc} \"">&3
     echo "python ../compile.py -o tester.s ${src[$i]}">&3
     echo "gcc -m32 -otest tester.s ../runtime/libruntime.a -lm">&3
     echo "echo 10 1 -5 -2 2 65 2 -0 -9 10 | ./test > file1">&3
     echo "echo 10 1 -5 -2 2 65 2 -0 -9 10 | python ${src[$i]} > file2">&3
     echo "diff file1 file2 > file3">&3
-    echo 'if [ '\$''\?' ] '>&3
+    echo 'if [ '\$''\?' == 0 ] '>&3
     echo 'then'>&3
-    echo -e "\techo \"${src[$i]} \": passed">&3
+    echo "    printf '  %-10s: %10s\n' \"\${green}${src[$i]}\" \"[PASS] \${nc}\" ">&3
     echo 'else'>&3
-    echo -e'\t echo failed'>&3
+    echo "    printf '  %-10s: %10s\n' \"\${red}${src[$i]}\" \"[FAIL] \${nc}\" ">&3
+    echo "    echo \"We got: \" ">&3
+    echo "    cat file1">&3
+    echo "    echo \"Should be: \" ">&3
+    echo "    cat file2">&3
+    echo "    echo -e"\n"">&3
+    
     echo 'fi'>&3
+    echo 'echo -e "\n"'>&3
     echo -e "\n">&3
 done
 
