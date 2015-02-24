a337cba5a2bdf50f37e313f934dda46c7ec7513c
nbroeking <nbroeking@me.com>
2015-02-23 22:11:53 -0700

    Fixed walrus

--- backslashx90/maketests.sh
+++ backslashx90/maketests.sh
@@ -3,7 +3,7 @@ files=$(find tests -name '*.py' -and -not -name 'onsite*')
 for i in $files ; do
     infile=$(echo $i | sed 's/py$/in/')
     if [ ! -e $infile ] ; then
-        echo -e '1\n2\n3\n4\n5\n6' > $infile
+        echo -e '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17' > $infile
     fi
 done
 
