78aa78c15e8da2beabf98101d48982c6c8243ebd
Devman <devel@archaen>
2015-04-15 17:45:57 -0600

    Syncing

--- Project/example_2.py
+++ Project/example_2.py
@@ -1,2 +1,17 @@
-def example(l):
-    return [f(x) for x in l if b(x)]
+def f():
+    x = 0
+    while(x!=4):
+        x+=1
+    return x
+
+def b():
+    x = 0
+    while(x!=400):
+        x+=1
+    return x
+
+
+y = f()
+z = b()
+
+print y + z
