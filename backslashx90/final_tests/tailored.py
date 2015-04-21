e4e40f4002ce6e6558734faec52cc338e0d2f2a0
Devman <devel@archaen>
2015-04-20 22:40:39 -0600

    One more Final

---
+++ backslashx90/final_tests/tailored.py
@@ -0,0 +1,81 @@
+def pure(q):
+    i = 1
+    while i != q:
+        i = i + 1
+    i = 1
+    while i != q:
+        i = i + 1
+    i = 1
+    while i != q:
+        i = i + 1
+    i = 1
+    while i != q:
+        i = i + 1
+    i = 1
+    while i != q:
+        i = i + 1
+    i = 1
+    while i != q:
+        i = i + 1
+    i = 1
+    while i != q:
+        i = i + 1
+    return i
+
+
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+a1 =  x + y + z + w + v + t
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+a2 = x + y + z + w + v + t
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+a3 = x + y + z + w + v + t
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+a4 = x + y + z + w + v + t
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+a5 = x + y + z + w + v + t
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+a6 = x + y + z + w + v + t
+x = pure(1000000)
+y = pure(1000000)
+z = pure(1000000)
+w = pure(1000000)
+v = pure(1000000)
+t = pure(1000000)
+
+print a1 + a2 + a3 + a4 +- a5 +- a6 + x + y + z + w + v + t
