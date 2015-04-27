3480236a378d21c551cd8de9efa506b141c1b835
Josh Rahm <joshuarahm@gmail.com>
2015-04-26 20:11:36 -0600

    more

--- backslashx90/bench.py
+++ backslashx90/bench.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-runtimes = {}
+totalrt = 0
 
 times = []
 diffs = []
@@ -14,33 +14,12 @@ for root, _, files in os.walk('final_tests/'):
             os.system('./build.sh %s/%s /tmp/testexe 2>/dev/null' % (root, f))
             print ("Running /tmp/testexe")
             start = time.time() 
-            os.system('cat input.txt | /tmp/testexe > /dev/null')
+            os.system('/tmp/testexe < input.txt > /dev/null')
             end = time.time()
 
             print ("Execution time: %fms" % ((end - start) * 1000.0))
 
-            runtimes[f] = end-start
+            totalrt += end-start
 
 
-os.system('git checkout hw6')
-
-os.system('make -f mk notests')
-for root, _, files in os.walk('final_tests/'):
-    for f in files:
-        if f.endswith('.py'):
-            os.system('./build.sh %s/%s /tmp/testexe 2>/dev/null' % (root, f))
-            start = time.time() 
-            os.system('cat input.txt | /tmp/testexe > /dev/null')
-            end = time.time()
-
-            print ("Execution time: %fms" % ((end - start) * 1000.0))
-            dif = runtimes[f] / (end-start)
-            print ("%s %s \x1b[01;32m%f\x1b[00;0m" % (f, " " * (100 - len(f)), dif))
-            diffs.append(dif)
-
-print ("Average: " + str(sum(diffs)/len(diffs)))
-
-
-
-os.system('git checkout master')
-
+print "Total " + str(totalrt)
